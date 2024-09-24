3f7bd7fc476932abce456d54a5d3328732e29b6c
Sam Hall <samuel.hall@helmholtz-berlin.de>
2024-09-24 14:26:53 +0200

    Remove schnet functions

--- utils/model.py
+++ utils/model.py
@@ -29,33 +29,7 @@ def train_model(model, loader, optimizer, device):
 
         optimizer.step()
 
-        # out = pred[0].detach().cpu().numpy()
-        # true = data.spectrum[0].detach().cpu().numpy()
-
-    return total_loss #, embedding#, true, out
-
-def train_schnet(model, loader, optimizer, device):
-    '''
-    '''
-
-    model.train()
-
-    total_loss = 0
-
-    for data in loader:
-        data = data.to(device)
-        optimizer.zero_grad()
-        z = data.z
-        pos = data.x.float()
-        pred = model(z, pos, data.batch)
-        # data_size = data.spectrum.shape[0] // 200
-        # data.spectrum = data.spectrum.view(data_size, 200)
-        loss = nn.MSELoss()(pred, data.spectrum).float()
-        total_loss += loss.item() * data.num_graphs
-        loss.backward()
-        optimizer.step()
-
-        return total_loss / len(loader.dataset)
+    return total_loss
 
 def val_test(model, loader, device):
     '''
@@ -79,23 +53,6 @@ def val_test(model, loader, device):
         total_loss += loss.item() / data.num_graphs
 
     return total_loss
-
-def val_schnet(model, loader, device):
-    '''
-    '''
-
-    model.eval()
-    total_loss = 0
-    for data in loader:
-        data = data.to(device)
-        z = data.z
-        pos = data.x.float()
-        pred = model(z, pos, data.batch)
-        # data_size = data.spectrum.shape[0] // 200
-        # data.spectrum = data.spectrum.view(data_size, 200)
-        loss = nn.MSELoss()(pred, data.spectrum)
-        total_loss += loss.item() * data.num_graphs
-        return total_loss / len(loader.dataset)
 
 
 def get_spec_prediction(model, index, dataset, device):
